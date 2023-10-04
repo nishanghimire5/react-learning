943d29e457011912ae52d0d019a66421071ee3ea
Nishan Ghimire <nishanghimire5@gmail.com>
2023-10-04 21:49:12 +1100

    //

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+    return(
+        <div>
+<h1>my custon app ! </h1>
+
+        </div>
+    )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <MyAppApp />
  
 )
